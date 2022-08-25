fe6e358a96bd70dc50f0f3e487954aa26ab50d47
Vishalk-1 <90751759+Vishalk-1@users.noreply.github.com>
2022-08-25 18:00:38 +0530

    Time: 26 ms (52.04%), Space: 9 MB (15.41%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+       map<char,int>mp;
+        for(int i=0;i<magazine.length();i++){
+            mp[magazine[i]]++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            if(mp[ransomNote[i]]==0)
+                return false;
+            mp[ransomNote[i]]--;
+        }
+        return true;
+    }
+};
