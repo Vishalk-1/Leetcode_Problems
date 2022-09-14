74c825acd042355e166746c1c99bdd2b4c5f1e9b
Vishalk-1 <90751759+Vishalk-1@users.noreply.github.com>
2022-09-14 16:14:19 +0530

    Time: 186 ms (10.97%), Space: 43.8 MB (52.41%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let ans=0;
+    while(ans*ans<=x){
+        ans++;
+        
+    }
+    return ans-1;
+    
+};
