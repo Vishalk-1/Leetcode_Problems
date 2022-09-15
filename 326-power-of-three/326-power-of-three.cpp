d16999342c7991fcaaf5201082523292607dea1c
Vishalk-1 <90751759+Vishalk-1@users.noreply.github.com>
2022-09-15 12:52:58 +0530

    Time: 18 ms (91.80%), Space: 5.9 MB (31.17%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -4,8 +4,7 @@ public:
         if(n<=0) return false;
         if(n==1)  return true;
         if(n%3!=0) 
-            return false;
-        
+            return false;  
         return isPowerOfThree(n/3);
     }
 };
