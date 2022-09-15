fc3f294195d1558abf015a757fb8116008a28602
Vishalk-1 <90751759+Vishalk-1@users.noreply.github.com>
2022-09-15 12:52:46 +0530

    Time: 36 ms (43.77%), Space: 5.9 MB (73.35%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        int ans=1;
-        if(n<=0){
+        if(n<=0) return false;
+        if(n==1)  return true;
+        if(n%3!=0) 
             return false;
-        }
-        if(n==1) return true;
-        for(int i=1;i<=30;i++){
-              if(ans==n){
-                return true;
-            }
-            if(ans<INT_MAX/2)
-            ans=ans*3;
-          
-        }
-        return false;
+        
+        return isPowerOfThree(n/3);
     }
 };
