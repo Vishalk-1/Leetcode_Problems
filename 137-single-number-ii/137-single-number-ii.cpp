7ce17eb49788ca943214bb38867e84b096f9156e
Vishalk-1 <90751759+Vishalk-1@users.noreply.github.com>
2022-09-18 00:12:14 +0530

    Time: 30 ms (5.07%), Space: 10.2 MB (15.22%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> mp;
+        int x=0;
+        for(auto it:nums){
+            mp[it]++;
+        }
+        for(auto it:mp){
+            if(it.second==1){   
+              x=  it.first;
+                break;
+            }
+        }
+        return x;
+        
+        
+    }
+};
