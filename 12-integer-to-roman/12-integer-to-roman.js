bf9447ab802008c923cee0c798c903ff41495746
Vishalk-1 <90751759+Vishalk-1@users.noreply.github.com>
2022-09-27 15:42:20 +0530

    Time: 128 ms (94.39%), Space: 46.5 MB (95.77%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    let roman = "";
+    const obj = {
+        M : 1000,
+        CM : 900,
+        D : 500,
+        CD : 400,
+        C : 100,
+        XC : 90,
+        L : 50,
+        XL : 40,
+        X : 10,
+        IX : 9,
+        V : 5,
+        IV : 4,
+        I : 1
+    }
+    for( let i in obj){
+        while(num >= obj[i]){
+            roman += i;
+            num -= obj[i];
+        }
+    }return roman;
+};
