e12b47c8e2e51c0ea83e281028d2670af30c2819
Vishalk-1 <90751759+Vishalk-1@users.noreply.github.com>
2022-09-27 15:15:33 +0530

    Time: 114 ms (61.83%), Space: 44 MB (79.40%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.js
@@ -3,10 +3,8 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-     const MAX_NUMBER = 2147483648
-    const number = parseInt(s)
-    
+     const MAX_NUMBER = 2**31;
+    const number = parseInt(s)    
     if (!number) return 0
-    
     return Math.max(-MAX_NUMBER, Math.min(MAX_NUMBER-1, number))
 };
